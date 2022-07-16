61b9925bb580598573b8932e4b31ccac80c592ee
Nick Wylynko <nick.wylynko@navitas.com>
2022-07-16 15:30:47 +0800

    fix deps

--- next.config.js
+++ next.config.js
@@ -1,13 +1,11 @@
-module.exports = {
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  typescript: {
-    // !! WARN !!
-    // Dangerously allow production builds to successfully complete even if
-    // your project has type errors.
-    // !! WARN !!
-    ignoreBuildErrors: true,
-  },
+  swcMinify: true,
   compiler: {
     styledComponents: true,
   },
-};
+}
+
+module.exports = nextConfig
